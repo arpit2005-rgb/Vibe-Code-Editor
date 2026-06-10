2dfcd0964f7980c4800dd9b86fc9f3ff77b94e14
Arpit Pandey <arpitpandey122337@gmail.com>
2026-06-10 10:59:17 +0530

    02Database

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import { db } from "@/lib/db";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
