d92ab1de9d3f5a1c012d3f3a7221c1be03c50c43
Arpit Pandey <arpitpandey122337@gmail.com>
2026-08-08 18:43:09 +0530

    Fix dashboard server actions

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,4 @@
-import {
-  deleteProjectById,
-  duplicateProjectById,
-  editProjectById,
-  getAllPlaygroundForUser,
-} from "@/modules/dashboard/actions";
+import { getAllPlaygroundForUser } from "@/modules/dashboard/actions";
 import AddNewButton from "@/modules/dashboard/components/add-new";
 import AddRepo from "@/modules/dashboard/components/add-repo";
 import EmptyState from "@/modules/dashboard/components/empty-state";
@@ -12,24 +7,19 @@ import React from "react";
 
 const Page = async () => {
   const playgrounds = await getAllPlaygroundForUser();
+
   return (
     <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
         <AddNewButton />
         <AddRepo />
       </div>
+
       <div className="flex flex-col justify-center items-center mt-10 w-full">
-        {playgrounds && playgrounds.length == 0 ? (
+        {playgrounds && playgrounds.length === 0 ? (
           <EmptyState />
         ) : (
-          <ProjectTable
-            projects={playgrounds || []}
-            onDeleteProject={deleteProjectById}
-            onUpdateProject={editProjectById}
-            onDuplicateProject={async (id) => {
-              await duplicateProjectById(id);
-            }}
-          />
+          <ProjectTable projects={playgrounds || []} />
         )}
       </div>
     </div>
